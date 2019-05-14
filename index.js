ef457c523d98583aa60ecb453a221bb54d26d29f
Felipe Garcia <lfcerero@misena.edu.co>
2019-05-14 18:52:41 -0500

    2019/05/14 prueba #2

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 //const {PORT}=require("../");
-const PORT = 3000;
+const PORT = process.env.PORT ||  3000;
 require("./routes/api") (app);
 require("./routes/views")(app);
 
 
 function init (){
   console.log ("inicializando ..");
+  response.end("Arrancando");
   app.listen(PORT, () =>{
     console.log ("esta activo ..");
+    response.end("]Go...");
   });
 }
 
