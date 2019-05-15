1444068301985de315841c6f05c7bdac9d4a842b
Felipe Garcia <lfcerero@misena.edu.co>
2019-05-14 19:55:52 -0500

    2019/05/14 prueba cinco

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+// });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+// const port = process.env.PORT || 1337;
+// server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+// console.log("Server running at http://localhost:%d", port);
 
-// var express = require('express');
-// var app = express();
-// //const {PORT}=require("../");
-// const PORT = process.env.PORT ||  3000;
-// require("./routes/api") (app);
-// require("./routes/views")(app);
+var express = require('express');
+var app = express();
+//const {PORT}=require("../");
+const PORT = process.env.PORT ||  3000;
+require("./routes/api") (app);
+require("./routes/views")(app);
 
 
-// function init (){
-//   console.log ("inicializando ..");
-//   response.end("Arrancando");
-//   app.listen(PORT, () =>{
-//     console.log ("esta activo ..");
-//     response.end("]Go...");
-//   });
-// }
+function init (){
+  console.log ("inicializando ..");
+  response.end("Arrancando");
+  app.listen(PORT, () =>{
+    console.log ("esta activo ..");
+    response.end("]Go...");
+  });
+}
 
-// init();
+init();
