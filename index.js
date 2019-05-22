e8eab3dbba79ba1001aef6ef16500754362713cd
Felipe Garcia <lfcerero@misena.edu.co>
2019-05-21 20:55:23 -0500

    2019/05/21 prueba 11

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+app.get('/',(req,res)=> res.send('lo logre!!!'));
+app.listen(port,()=>console.log('corriendo'));
 
-var express = require('express');
-var app = express();
-require("./routes/api") (app);
-require("./routes/views")(app);
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("];)!");
-});
+// var express = require('express');
+// var app = express();
+// require("./routes/api") (app);
+// require("./routes/views")(app);
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("];)!");
+// });
 
-console.log("Server running at http://localhost:%d", port);
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
 
 
 // // //const {PORT}=require("../");
