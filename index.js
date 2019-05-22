5d78e9d6a28721ca0520db837d35fe2bf83efba4
Felipe Garcia <lfcerero@misena.edu.co>
2019-05-21 21:10:01 -0500

    2019/05/21 prueba 12

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+require("./routes/api") (app);
+require("./routes/views")(app);
 var port = process.env.PORT || 3000;
 app.get('/',(req,res)=> res.send('lo logre!!!'));
 app.listen(port,()=>console.log('corriendo'));
 
-// const http = require('http');
 
+// const http = require('http');
 // var express = require('express');
 // var app = express();
-// require("./routes/api") (app);
-// require("./routes/views")(app);
+
 
 // const server = http.createServer((request, response) => {
 //     response.writeHead(200, {"Content-Type": "text/plain"});
